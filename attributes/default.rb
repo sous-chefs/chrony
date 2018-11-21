75be349ddf3a397cae0487f135e34d46319f9b9b
Sten Spans <sten@sp5.nl>
2018-11-21 14:03:04 +0100

    update default ntp pool servers

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,13 +17,36 @@
 # limitations under the License.
 #
 
-# hash of default servers in the chrony.conf from Ubuntu
-default['chrony']['servers'] = {
-  '0.debian.pool.ntp.org' => 'offline minpoll 8',
-  '1.debian.pool.ntp.org' => 'offline minpoll 8',
-  '2.debian.pool.ntp.org' => 'offline minpoll 8',
-  '3.debian.pool.ntp.org' => 'offline minpoll 8',
-}
+# hash of default servers in the chrony.conf
+case node['platform']
+when 'debian', 'ubuntu', 'centos'
+  default['chrony']['servers'] = {
+    "0.#{node['platform']}.pool.ntp.org" => 'offline minpoll 8',
+    "1.#{node['platform']}.pool.ntp.org" => 'offline minpoll 8',
+    "2.#{node['platform']}.pool.ntp.org" => 'offline minpoll 8',
+    "3.#{node['platform']}.pool.ntp.org" => 'offline minpoll 8',
+  }
+when 'rhel'
+  default['chrony']['servers'] = {
+    '0.rhel.pool.ntp.org' => 'offline minpoll 8',
+    '1.rhel.pool.ntp.org' => 'offline minpoll 8',
+    '2.rhel.pool.ntp.org' => 'offline minpoll 8',
+    '3.rhel.pool.ntp.org' => 'offline minpoll 8',
+  }
+else
+  default['chrony']['servers'] = {
+    '0.pool.ntp.org' => 'offline minpoll 8',
+    '1.pool.ntp.org' => 'offline minpoll 8',
+    '2.pool.ntp.org' => 'offline minpoll 8',
+    '3.pool.ntp.org' => 'offline minpoll 8',
+  }
+end
+
+if node['platform_family'] == 'rhel'
+  default['chrony']['service'] = 'chronyd'
+else
+  default['chrony']['service'] = 'chrony'
+end
 
 default['chrony']['server_options'] = 'offline minpoll 8'
 
