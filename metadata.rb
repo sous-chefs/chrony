c37fc0c1429fa1bd428add68d416d070a51b4738
Tim Smith <tsmith84@gmail.com>
2020-01-04 11:33:01 -0800

    Switch to GitHub Actions for testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'chrony'
 maintainer       'Chef Software, Inc..'
-maintainer_email 'cookbooks@@chef.io'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures chrony, an application used to maintain the accuracy of the system clock (similar to NTP).'
 version          '0.2.1'
