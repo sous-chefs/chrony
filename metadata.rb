decb1854611368dec51e7388a3253446aabe53ee
Tim Smith <tsmith84@gmail.com>
2020-02-10 13:45:42 -0800

    Minor updates to the kitchen files and metadata.rb source fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures chrony, an application used to maintain th
 
 version          '0.3.0'
 chef_version     '>= 13.0'
-issues_url 'https://github.com/chef-cookbook/chrony/issues'
-source_url 'https://github.com/chef-cookbook/chrony'
+issues_url       'https://github.com/chef-cookbooks/chrony/issues'
+source_url       'https://github.com/chef-cookbooks/chrony'
 
 supports 'debian'
 supports 'ubuntu'
