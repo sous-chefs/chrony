1b6df504c72576c830af3e2594b8ce983996ac90
John Beranek <john.beranek@pamediagroup.com>
2024-02-07 14:35:02 +0000

    fix: Define default['chrony']['servers'] in tests

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,3 +1,6 @@
 # Workaround issue running chronyd in docker related to:
 # https://access.redhat.com/solutions/4410831
 directory '/var/run/chrony' if platform_family?('rhel')
+
+default['chrony']['servers'] = { 'pool.ntp.org' => 'iburst' }
+
