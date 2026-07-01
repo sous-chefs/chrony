a53f9f9a81cd2ef1b92cf83d74c4f43f10afb673
Dan Webb <dan.webb@damacus.io>
2026-07-01 16:14:29 +0100

    fix: skip verification for chrony drop-in fragment

--- test/cookbooks/test/recipes/server.rb
+++ test/cookbooks/test/recipes/server.rb
@@ -25,6 +25,7 @@ if platform_family?('rhel') && node['platform_version'].to_i >= 10
       [Service]
       Type=simple
     UNIT
+    verify false
     action :create
     notifies :run, 'execute[systemctl daemon-reload]', :immediately
   end
