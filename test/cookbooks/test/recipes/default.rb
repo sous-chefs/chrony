05a2308339d274043a4889d06303c332910af11a
Dan Webb <dan.webb@damacus.io>
2026-07-01 11:25:43 +0100

    fix: stabilize el10 chrony fixtures

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -2,6 +2,17 @@
 
 apt_update
 
+if platform_family?('rhel') && node['platform_version'].to_i >= 10
+  package 'chrony'
+
+  file '/etc/sysconfig/chronyd' do
+    content %(# Command-line options for chronyd\nOPTIONS="-F 0"\n)
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+end
+
 chrony_config 'default' do
   extra_config ['log measurements statistics tracking']
 end
--- test/cookbooks/test/recipes/server.rb
+++ test/cookbooks/test/recipes/server.rb
@@ -2,6 +2,17 @@
 
 apt_update
 
+if platform_family?('rhel') && node['platform_version'].to_i >= 10
+  package 'chrony'
+
+  file '/etc/sysconfig/chronyd' do
+    content %(# Command-line options for chronyd\nOPTIONS="-F 0"\n)
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+end
+
 chrony_config 'server' do
   allow ['10.0.0.0/8']
 end
