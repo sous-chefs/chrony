cd4fa99cc82773bd66466901f449087984d53cb7
Dan Webb <dan.webb@damacus.io>
2026-07-01 11:13:04 +0100

    fix: stabilize chrony integration targets

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -2,14 +2,6 @@
 
 apt_update
 
-directory '/var/run/chrony' do
-  owner 'chrony'
-  group 'chrony'
-  mode '0755'
-  recursive true
-  only_if { platform_family?('rhel', 'fedora', 'amazon') }
-end
-
 chrony_config 'default' do
   extra_config ['log measurements statistics tracking']
 end
--- test/cookbooks/test/recipes/server.rb
+++ test/cookbooks/test/recipes/server.rb
@@ -2,14 +2,6 @@
 
 apt_update
 
-directory '/var/run/chrony' do
-  owner 'chrony'
-  group 'chrony'
-  mode '0755'
-  recursive true
-  only_if { platform_family?('rhel', 'fedora', 'amazon') }
-end
-
 chrony_config 'server' do
   allow ['10.0.0.0/8']
 end
